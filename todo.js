82e2bb2dd8f69fa7e1c54f5f1475bdc58292f041
samueldahl <samjoedahl@gmail.clom>
2017-11-08 13:41:36 -0700

    I created a mockup to model what I wanted my web page to look like. It looks pretty dank. I am now going over what functions I will need to create a usefull application n stuff.

---
+++ todo.js
@@ -0,0 +1,32 @@
+function addList(){
+
+}
+function clearCompleteLists(){
+
+}
+
+
+
+
+function addList() {
+    var btn = document.createElement("button");
+    document.body.appendChild(btn);
+}
+function addTask(){
+
+}
+var p = document.createElement('p');
+p.innerHTML = 'Hello world!';
+listBox.appendChild(p);
+
+
+
+
+
+
+
+//These lines were written by JD Flamm in an attempt to help my poor soul understand document.append().
+// var template = '<button onclick="' + index + '">dank memes</button>';
+// document.append();
+
+//html can be added as a string.

4b68397ef5f10effb33cc818c9e63bd98061f819
samueldahl <samjoedahl@gmail.clom>
2017-11-08 13:59:40 -0700

    I have named all the functions that I need and started their outlines, I have perfected my base HTML refrence document. Now its time for the heavy lifiting and the visits to w3 schools.

--- todo.js
+++ todo.js
@@ -1,3 +1,5 @@
+
+//For the list creator section
 function addList(){
 
 }
@@ -5,27 +7,48 @@ function clearCompleteLists(){
 
 }
 
-
-
-
-function addList() {
-    var btn = document.createElement("button");
-    document.body.appendChild(btn);
-}
+//For every actual list
 function addTask(){
 
 }
-var p = document.createElement('p');
-p.innerHTML = 'Hello world!';
-listBox.appendChild(p);
+function markListComplete(){
+
+}
+function deleteList(){
+
+}
+
+//For every actual task
+function editTask(){
+
+}
+function markTaskComplete(){
+
+}
+function deleteTask(){
+
+}
 
 
 
 
+//I'm not sure what these lines of code do, you can think of this as a reference dump.
+
+// function addList() {
+//     var btn = document.createElement("button");
+//     document.body.appendChild(btn);
+// }
+// function addTask(){
+//
+// }
+// var p = document.createElement('p');
+// p.innerHTML = 'Hello world!';
+// listBox.appendChild(p);
 
 
 
 //These lines were written by JD Flamm in an attempt to help my poor soul understand document.append().
+
 // var template = '<button onclick="' + index + '">dank memes</button>';
 // document.append();
 

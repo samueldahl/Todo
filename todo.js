3b58f66bfa58e12d4c0ceea1820989afd82fd595
samueldahl <samjoedahl@gmail.clom>
2017-11-09 19:53:20 -0700

    Removed some javascript comented code from my friend trying to help me understand a concept I ended up not using

--- todo.js
+++ todo.js
@@ -55,28 +55,3 @@ function markTaskComplete(taskId){
 function deleteTask(element){
     $(element).parent().hide();
 }
-
-
-
-
-//I'm not sure what these lines of code do, you can think of this as a reference dump.
-
-// function addList() {
-//     var btn = document.createElement("button");
-//     document.body.appendChild(btn);
-// }
-// function addTask(){
-//
-// }
-// var p = document.createElement('p');
-// p.innerHTML = 'Hello world!';
-// listBox.appendChild(p);
-
-
-
-//These lines were written by JD Flamm in an attempt to help my poor soul understand document.append().
-
-// var template = '<button onclick="' + index + '">dank memes</button>';
-// document.append();
-
-//html can be added as a string.
